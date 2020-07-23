157530c0939399d3f3ed1f8d106e8a019fe994c9
sungjin-shin <sungjin15066@naver.com>
2020-07-24 02:57:51 +0900

    feat: 코스피 200 회사들 과거 주가 크롤링
    feat: 코스피 200 1페이지 회사 과거(10일) 주가 크롤링
    
    feat: 코스피 200 회사들의 과거(10)일 주가 크롤링
    
    이름없음 ㅠㅠ

--- crawling.py
+++ crawling.py
@@ -1,43 +1,48 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 
 def crawler():
     total = []
     for page_num in range(1, 21):
-        result = crawler_one_page(page_num)
+        result = crawler_total_past_prices(page_num)
         total = total + result
     return total
 
 
-def crawler_one_page(page_num):
+def crawler_total_past_prices(page_num):
     url = 'https://finance.naver.com/sise/entryJongmok.nhn?&page=' + str(page_num)
     html = requests.get(url)
     soup = BeautifulSoup(html.text, 'html.parser')
-    company_names = soup.find_all("td", {"class": "ctg"})
-    stock_prices = soup.find_all("td", {"class": "number_2"})
-    total_company = find_company_name(company_names)
-    total_prices = find_stock_price(stock_prices)
-    return list(zip(total_company, total_prices))
+    company_codes = soup.find_all("td", {"class": "ctg"})
+
+    total_past_prices = []
+    for company_code in company_codes:
+        total_past_prices.append(crawler_one_past_prices(company_code.a['href'].split('=')[1]))
+    return total_past_prices
 
 
-def find_company_name(company_names):
-    total_company = []
-    for company_name in company_names:
-        # print(company_name.text.strip())
-        total_company.append(company_name.text.strip())
-    return total_company
+def crawler_one_past_prices(code):
+    url = 'https://finance.naver.com/item/sise_day.nhn?code=' + code + '&page=' + str(1)
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    company_past_prices = soup.find_all("td", {"class": "num"})
 
-
-def find_stock_price(stock_prices):
-    total_prices = []
-    for index, stock_price in enumerate(stock_prices):
-        if index % 4 == 0:
-            # print(stock_price.text.strip())
-            total_prices.append(int(stock_price.text.strip().replace(',', '')))
-    return total_prices
+    past_prices = []
+    for index, company_past_price in enumerate(company_past_prices):
+        if index % 6 == 0:
+            past_prices.append(company_past_price.text.strip().replace(',', ''))
+    return past_prices
 
 
 if __name__ == "__main__":
-    kospi200 = crawler()
-    print(kospi200)
+    f = open('crawled_data.csv', 'w', encoding='utf-8')
+    wr = csv.writer(f)
+    result_data = crawler()
+    wr.writerows(result_data)
+    print('끝남')
+    """
+    1. 모든 페이지에서 회사의 code값들을 모아야겠군
+    2. 모은 code값으로 들어가서 회사의 과거 주식값들을 모아야겠군
+    """
