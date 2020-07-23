6ff74ada99cdf1f2c68db10b052c9609f5cfea59
sungjin-shin <sungjin15066@naver.com>
2020-07-24 01:44:07 +0900

    feat: 코스피 200 주가 크롤러 구현
    모든 페이지에 대한 크롤링 함수 구현

--- crawling.py
+++ crawling.py
@@ -3,40 +3,41 @@ from bs4 import BeautifulSoup
 
 
 def crawler():
-    url = 'https://finance.naver.com/sise/entryJongmok.nhn?&page=2'
+    total = []
+    for page_num in range(1, 21):
+        result = crawler_one_page(page_num)
+        total = total + result
+    return total
+
+
+def crawler_one_page(page_num):
+    url = 'https://finance.naver.com/sise/entryJongmok.nhn?&page=' + str(page_num)
     html = requests.get(url)
     soup = BeautifulSoup(html.text, 'html.parser')
-    # print(soup)
     company_names = soup.find_all("td", {"class": "ctg"})
     stock_prices = soup.find_all("td", {"class": "number_2"})
-    # print(soup.body)
+    total_company = find_company_name(company_names)
+    total_prices = find_stock_price(stock_prices)
+    return list(zip(total_company, total_prices))
 
+
+def find_company_name(company_names):
     total_company = []
     for company_name in company_names:
-        print(company_name.text.strip())
+        # print(company_name.text.strip())
         total_company.append(company_name.text.strip())
-    print(total_company)
+    return total_company
 
+
+def find_stock_price(stock_prices):
     total_prices = []
     for index, stock_price in enumerate(stock_prices):
         if index % 4 == 0:
-            print(stock_price.text.strip())
+            # print(stock_price.text.strip())
             total_prices.append(int(stock_price.text.strip().replace(',', '')))
-    print(total_prices)
-
-    total = []
-    total = list(zip(total_company, total_prices))
-    print(total)
+    return total_prices
 
 
-    # select = soup.head.find_all('meta')
-    # for meta in select:
-    #     print(meta.get('content'))
-
-
-# <td class="ctg"><a href="/item/main.nhn?code=028260" target="_parent">삼성물산</a></td>
-
-# <meta content="The official home of the Python Programming Language" name="description"/>
-# print (bsObject.head.find("meta", {"name":"description"}))
-
-crawler()
+if __name__ == "__main__":
+    kospi200 = crawler()
+    print(kospi200)
