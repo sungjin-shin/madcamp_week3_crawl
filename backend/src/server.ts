8d531e4eb16748a3228c7cf0d4b1ca0f27299fbc
JongWoo Park <jjongwoo.dev@gmail.com>
2020-07-26 01:25:42 +0900

    feat: 프론트엔드 server 추가

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import * as Morgan from "morgan";
 import * as cors from "cors";
 import * as session from "express-session";
 import route from "./routes";
+import views from "./views";
 import { createConnection } from "typeorm";
 
 const app = Express();
@@ -21,6 +22,11 @@ const initialize = async (): Promise<void> => {
 
 initialize();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+app.use("/static", Express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(Morgan("dev"));
@@ -33,7 +39,8 @@ app.use(
   })
 );
 
-app.use("/", route);
+app.use("/", views);
+app.use("/api", route);
 
 app.use(function (req, res, next) {
   res.status(404).send("Sorry! 존재하지 않는 API이에오!");
