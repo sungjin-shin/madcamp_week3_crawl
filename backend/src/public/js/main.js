fb3da7a4b1042091ac09ca1e4f4d09329f1eeab3
sungjin-shin <sungjin15066@naver.com>
2020-07-28 15:24:30 +0900

    UI design version merged

--- backend/src/public/js/main.js
+++ backend/src/public/js/main.js
@@ -2,54 +2,54 @@ const BASE_URL = "http://192.249.19.243:8780/";
 // const BASE_URL = "http://localhost:8081/";
 
 $(document).ready(function () {
-  $("#sidebarCollapse").on("click", function () {
-    $("#sidebar").toggleClass("active");
-  });
-});
-
-$(document).ready(function () {
-  $("#rightsidebarCollapse").on("click", function () {
-    $("#rightsidebar").toggleClass("active");
-  });
-});
-
-$(document).ready(function () {
-  $("#listSearch").on("keyup", function () {
-    var value = $(this).val().toLowerCase();
-    var cnt = 0;
-    $("#myList a").show();
-    $("#myList a").filter(function () {
-      var isMatched = $(this).text().toLowerCase().indexOf(value) > -1;
-      $(this).toggle(isMatched);
-      if (isMatched) cnt++;
-      if (cnt > 10) {
-        $(this).hide();
-      }
+    $("#sidebarCollapse").on("click", function () {
+        $("#sidebar").toggleClass("active");
     });
-  });
 });
 
 $(document).ready(function () {
-  $("#tableSearch").on("keyup", function () {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    $("#rightsidebarCollapse").on("click", function () {
+        $("#rightsidebar").toggleClass("active");
     });
-  });
 });
 
 $(document).ready(function () {
-  $("#logoutBtn").on("click", function () {
-    console.log("로그아웃 시도");
-    doLogout();
-  });
+    $("#listSearch").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        var cnt = 0;
+        $("#myList label").show();
+        $("#myList label").filter(function () {
+            var isMatched = $(this).text().toLowerCase().indexOf(value) > -1;
+            $(this).toggle(isMatched);
+            if (isMatched) cnt++;
+            if (cnt > 10) {
+                $(this).hide();
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $("#tableSearch").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        });
+    });
+});
+
+$(document).ready(function () {
+    $("#logoutBtn").on("click", function () {
+        console.log("로그아웃 시도");
+        doLogout();
+    });
 });
 
 function doLogout() {
-  fetch(BASE_URL + "api/auth/logout")
-    .then((response) => response.json())
-    .then((data) => {
-      $(location).attr("href", BASE_URL);
-    })
-    .catch((err) => console.error(err));
+    fetch(BASE_URL + "api/auth/logout")
+        .then((response) => response.json())
+        .then((data) => {
+            $(location).attr("href", BASE_URL);
+        })
+        .catch((err) => console.error(err));
 }
--- backend/src/public/js/vis.js
+++ backend/src/public/js/vis.js
@@ -4,185 +4,174 @@ var edges = new vis.DataSet([]);
 var selComp = [];
 var compInfos = [];
 function draw() {
-  console.log("drawing");
-  var container = document.getElementById("mynetwork");
-  var data = {
-    nodes: nodes,
-    edges: edges,
-  };
-  var options = {
-    nodes: {
-      shape: "dot",
-      scaling: {
-        min: 20,
-        max: 30,
-      },
-    },
-    layout: {
-      randomSeed: 34,
-    },
-    physics: {
-      forceAtlas2Based: {
-        gravitationalConstant: -26,
-        centralGravity: 0.005,
-        springLength: 230,
-        springConstant: 0.18,
-      },
-      maxVelocity: 146,
-      solver: "forceAtlas2Based",
-      timestep: 0.35,
-      stabilization: {
-        enabled: true,
-        iterations: 2000,
-        updateInterval: 25,
-      },
-    },
-  };
-  var network = new vis.Network(container, data, options);
+    console.log("drawing");
+    var container = document.getElementById("mynetwork");
+    var data = {
+        nodes: nodes,
+        edges: edges,
+    };
+    var options = {
+        nodes: {
+            shape: "dot",
+            scaling: {
+                min: 20,
+                max: 30,
+            },
+        },
+        layout: {
+            randomSeed: 34,
+        },
+        physics: {
+            forceAtlas2Based: {
+                gravitationalConstant: -500,
+                centralGravity: 0.005,
+                springLength: 230,
+                springConstant: 0.18,
+            },
+            maxVelocity: 146,
+            solver: "forceAtlas2Based",
+            timestep: 0.35,
+            stabilization: {
+                enabled: true,
+                iterations: 2000,
+                updateInterval: 25,
+            },
+        },
+    };
+    var network = new vis.Network(container, data, options);
 
-  network.on("stabilizationProgress", function (params) {
-    var maxWidth = 496;
-    var minWidth = 20;
-    var widthFactor = params.iterations / params.total;
-    var width = Math.max(minWidth, maxWidth * widthFactor);
+    network.on("stabilizationProgress", function (params) {
+        var maxWidth = 496;
+        var minWidth = 20;
+        var widthFactor = params.iterations / params.total;
+        var width = Math.max(minWidth, maxWidth * widthFactor);
 
-    document.getElementById("bar").style.width = width + "px";
-    document.getElementById("text").innerHTML =
-      Math.round(widthFactor * 100) + "%";
-  });
+    });
 
-  network.on("doubleClick", (params) => {
-    params.event = "[original Event]";
-    var nodeInfo = params;
-    node = nodes.get(nodeInfo.nodes);
-    var compName = node[0].label;
-    console.log(node);
-    $("#resultChecker").text(compName);
-    compNews(compName);
-    if ($("#rightsidebar").attr("class") != "") {
-      $("#rightsidebar").toggleClass("active");
-    }
-  });
+    network.on("doubleClick", (params) => {
+        params.event = "[original Event]";
+        var nodeInfo = params;
+        node = nodes.get(nodeInfo.nodes);
+        var compName = node[0].label;
+        console.log(node);
+        $("#resultChecker").text(compName);
+        compNews(compName);
+        if ($("#rightsidebar").attr("class") != "") {
+            $("#rightsidebar").toggleClass("active");
+        }
+    });
 
-  network.once("stabilizationIterationsDone", function () {
-    document.getElementById("text").innerHTML = "100%";
-    document.getElementById("bar").style.width = "496px";
-    document.getElementById("loadingBar").style.opacity = 0;
-    // really clean the dom element
-    setTimeout(function () {
-      document.getElementById("loadingBar").style.display = "none";
-    }, 500);
-  });
 }
 
 function drawGraph(ev) {
-  console.log("hit");
-  console.log(ev.getAttribute("value"));
+    console.log("hit");
+    console.log(ev.getAttribute("value"));
 
-  if (
-    selComp.find((element) => element == ev.getAttribute("value")) == undefined
-  ) {
-    console.log(selComp.find((element) => element == ev.getAttribute("value")));
-    selComp.push(ev.getAttribute("value"));
-    selComp = Array.from(new Set(selComp));
-    console.log(selComp);
-    sendSelComp();
-    setTimeout(draw, 50);
-  } else {
-    var idx = selComp.indexOf(ev.getAttribute("value"));
-    selComp.splice(idx, 1);
-    sendSelComp();
-    setTimeout(draw, 50);
-  }
+    if (
+        selComp.find((element) => element == ev.getAttribute("value")) == undefined
+    ) {
+        console.log(selComp.find((element) => element == ev.getAttribute("value")));
+        selComp.push(ev.getAttribute("value"));
+        selComp = Array.from(new Set(selComp));
+        console.log(selComp);
+        sendSelComp();
+        setTimeout(draw, 50);
+    } else {
+        var idx = selComp.indexOf(ev.getAttribute("value"));
+        selComp.splice(idx, 1);
+        sendSelComp();
+        setTimeout(draw, 50);
+    }
 }
 
 function sendSelComp() {
-  postData(BASE_URL + "api/company/corr", {
-    companies: selComp,
-  })
-    .then((data) => {
-      nodes = new vis.DataSet(data.data.nodes);
-      edges = new vis.DataSet(data.data.edges);
+    postData(BASE_URL + "api/company/corr", {
+        companies: selComp,
     })
-    .catch((error) => console.error(error));
+        .then((data) => {
+            nodes = new vis.DataSet(data.data.nodes);
+            edges = new vis.DataSet(data.data.edges);
+        })
+        .catch((error) => console.error(error));
 }
 
 function compInfo() {
-  getData(BASE_URL + "api/company/list", {})
-    .then((data) => {
-      for (comps in data.data) {
-        compInfos.push({
-          회사명: data.data[comps].name,
-          업종: data.data[comps].upjong,
-          시가총액: data.data[comps].sichong,
-        });
-      }
+    getData(BASE_URL + "api/company/list", {})
+        .then((data) => {
+            for (comps in data.data) {
+                compInfos.push({
+                    회사명: data.data[comps].name,
+                    업종: data.data[comps].upjong,
+                    시가총액: data.data[comps].sichong,
+                });
+            }
 
-      return true;
-    })
-    .then(() => {
-      putCompanyList();
-    })
-    .catch((error) => console.error(error));
+            return true;
+        })
+        .then(() => {
+            putCompanyList();
+        })
+        .catch((error) => console.error(error));
 }
 function preloadUserCompany() {
-  getData(BASE_URL + "api/company/getcompanys")
-    .then((response) => {
-      //   console.log(`type: ${typeof response.data}`);
-      //   console.log(`type: ${JSON.stringify(response.data)}`);
-      selComp = response.data;
-      console.log(`user Company: ${selComp}`);
-      sendSelComp();
-      setTimeout(draw, 100);
-    })
-    .catch((error) => console.error(error));
+    getData(BASE_URL + "api/company/getcompanys")
+        .then((response) => {
+            //   console.log(`type: ${typeof response.data}`);
+            //   console.log(`type: ${JSON.stringify(response.data)}`);
+            selComp = response.data;
+            console.log(`user Company: ${selComp}`);
+            sendSelComp();
+            setTimeout(draw, 100);
+        })
+        .catch((error) => console.error(error));
 }
 
 function postData(url, data) {
-  console.log(data);
-  return fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).then((response) => response.json());
+    console.log(data);
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    }).then((response) => response.json());
 }
 
 function getData(url, data) {
-  //console.log(data);
-  return fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((response) => response.json());
+    //console.log(data);
+    return fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((response) => response.json());
 }
 
 function putCompanyList() {
-  compInfos.forEach((element) => {
-    $("#myList").append(
-      `<a class="list-group-item" href="#" onclick="drawGraph(this)" value="${element["회사명"]}" > <span class="fa fa-check" id="isSelected" display = "none"></span> ${element["회사명"]}, ${element["시가총액"]}</a>`
-    );
-  });
+    compInfos.forEach((element) => {
+        $("#myList").append(
+            //   `<a class="list-group-item" href="#" onclick="drawGraph(this)" value="${element["회사명"]}" > <span class="fa fa-check" id="isSelected" display = "none"></span> ${element["회사명"]}, ${element["시가총액"]}</a>`
+            `<label><div class="list-group-item" href="#" ><input type="checkbox" onclick="drawGraph(this)" value="${element["회사명"]}"  />${element["회사명"]}, ${element["시가총액"]}</div></label>`
+        );
+    });
 }
 function compNews(compName) {
-  postData(BASE_URL + "api/naver/news", { comp: compName })
-    .then((data) => {
-      putCompanyNews(data.data);
-      //console.log(compInfos);
-      //console.log(JSON.stringify(data));
-      return true;
-    })
-    .catch((error) => console.error(error));
+    postData(BASE_URL + "api/naver/news", { comp: compName })
+        .then((data) => {
+            putCompanyNews(data.data);
+            //console.log(compInfos);
+            //console.log(JSON.stringify(data));
+            return true;
+        })
+        .catch((error) => console.error(error));
 }
 
 function putCompanyNews(data) {
-  $("#newsList").empty();
-  console.log(data);
-  data.forEach((element) => {
-    $("#newsList").append(
-      `<div class="card custom-card">
+    $("#newsList").empty();
+    console.log(data);
+    data.forEach((element) => {
+        $("#newsList").append(
+            `<div class="card custom-card">
         <div class="card-body">
           <h5 class="card-title">
           ${element["title"]}
@@ -200,11 +189,11 @@ function putCompanyNews(data) {
           >
         </div>
       </div>`
-    );
-  });
+        );
+    });
 }
 window.onload = function () {
-  console.log("load가 다 됐어요.");
-  compInfo();
-  preloadUserCompany();
+    console.log("load가 다 됐어요.");
+    compInfo();
+    preloadUserCompany();
 };
