5a7f51bd9f2726c2ffa7091ba37b054c04d1c5ae
JongWoo Park <jjongwoo.dev@gmail.com>
2020-07-28 16:45:27 +0900

    feat: 회원가입 추가

---
+++ backend/src/public/js/reigster.js
@@ -0,0 +1,72 @@
+window.onload = function () {
+  const Api = new apiController();
+
+  $(".form-register").on("submit", registerHandler);
+};
+class apiController {
+  constructor() {
+    this.url = "http://192.249.19.243:8780/api/";
+  }
+  register(data) {
+    const apiPath = "auth/register";
+    return this._postData(apiPath, data);
+  }
+  async _postData(apiPath, data) {
+    return fetch(this.url + apiPath, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.status);
+  }
+}
+function registerHandler(event) {
+  event.preventDefault();
+  const email = $("#inputEmail").val();
+  const password1 = $("#inputPassword").val();
+  const password2 = $("#inputPassword2").val();
+  const name = $("#inputName").val();
+
+  const registerForm = {
+    email,
+    password1,
+    password2,
+    name,
+  };
+  if (validationInput(registerForm)) {
+    //TODO: 회원가입 함수로 빼기
+    Api.register({ email, password: password1, name }).then((status) => {
+      console.log("status: " + status);
+      if (status == 200) {
+        alert("회원가입 성공");
+        $(location).attr("href", "http://192.249.19.243:8780/");
+      } else {
+        alert("회원가입 실패");
+        location.reload(true);
+      }
+    });
+  } else {
+    alert("비밀번호가 일치하지 않습니다.");
+  }
+}
+function validationInput(registerForm) {
+  const { email, password1, password2, name } = registerForm;
+  if (
+    email.length > 0 &&
+    password1.length > 0 &&
+    password2.length > 0 &&
+    name.length > 0 &&
+    confirmPassword(password1, password2)
+  )
+    return true;
+  else return false;
+}
+
+function confirmPassword(password1, password2) {
+  if (password1 != password2) {
+    return false;
+  } else {
+    return true;
+  }
+}
--- backend/src/views/index.ts
+++ backend/src/views/index.ts
@@ -15,20 +15,15 @@ router.get("/main", function (req, res) {
   if (!req.session.user) {
     return res.status(403).redirect("/");
   } else {
-    const email = req.session.user.email;
-    UserCompanys.findOne(email)
-      .then((companys) => {
-        if (companys == null)
-          return res.render("main", { selectedCompanies: [] });
-        else
-          return res.render("main", {
-            selectedCompanies: companys.companyNames,
-          });
-      })
-      .catch((err) => {
-        console.error(err);
-        return res.render("main", { selectedCompanies: [] });
-      });
+    return res.render("main");
+  }
+});
+
+router.get("/register", function (req, res) {
+  if (req.session.user) {
+    return res.render("main");
+  } else {
+    return res.render("register");
   }
 });
 
